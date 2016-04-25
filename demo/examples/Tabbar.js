a95e322aa12fe8b50c87cc85b13af5f7a95bd291
Patrick Klitzke <philologos14@gmail.com>
2016-04-25 16:06:07 +0900

    tabbar modifications

--- demo/examples/Tabbar.js
+++ demo/examples/Tabbar.js
@@ -4,33 +4,46 @@ import {Tabbar, Tab, Page, Button} from 'react-onsenui';
 import MyToolbar from './MyToolbar';
 
 class TabPage extends React.Component {
-  switchTab() {
-    const index = this.props.tabbar.getActiveTabIndex();
-    this.props.tabbar.setActiveTab((index + 1) % 2);
-  }
 
   render() {
+    console.log('render tab: ' + this.props.index + ' ' +  this.props.active);
     return (
       <Page renderToolbar={() => <MyToolbar title={this.props.title} />} >
-        {this.props.active ?
-          <p>This is the <strong>{this.props.title}</strong> page.</p> : null}
+        {this.props.active
+          ? <p>This is the <strong>{this.props.title}</strong> page.</p> : null}
 
-        <Button onClick={this.switchTab.bind(this)}>Go to the other tab</Button>
+          <Button onClick={() => {this.props.onSwitchClick()}}>Go to the other tab</Button>
       </Page>
     );
   }
 }
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderTabs = this.renderTabs.bind(this);
+    this.switchTab = this.switchTab.bind(this);
+    this.state = {
+      activeIndex: 0
+    };
+  }
+
+  switchTab() {
+    console.log('switch tab');
+    this.setState({
+      activeIndex: (this.state.activeIndex + 1) % 2
+    });
+  }
+
   renderTabs(activeIndex, tabbar) {
     return [
       {
-        content: <TabPage title="Home" active={activeIndex === 0} tabbar={tabbar} />,
-        tab: <Tab label="Home" icon="md-home" />
+        content: <TabPage onSwitchClick={this.switchTab} index={0} title='Home' active={activeIndex === 0} tabbar={tabbar} />,
+        tab: <Tab label='Home' icon='md-home' />
       },
       {
-        content: <TabPage title="Settings" active={activeIndex === 1} tabbar={tabbar} />,
-        tab: <Tab label="Settings" icon="md-settings" />
+        content: <TabPage onSwitchClick={this.switchTab} index={1} title='Settings' active={activeIndex === 1} tabbar={tabbar} />,
+        tab: <Tab label='Settings' icon='md-settings' />
       }
     ];
   }
@@ -39,6 +52,7 @@ export default class extends React.Component {
     return (
       <Page>
         <Tabbar
+          activeIndex={this.state.activeIndex}
           onPreChange={() => console.log('preChange')}
           onPostChange={() => console.log('postChange')}
           onReactive={() => console.log('postChange')}
--- src/components/Tabbar.jsx
+++ src/components/Tabbar.jsx
@@ -4,19 +4,13 @@ import Util from './Util.js';
 
 class Tabbar extends BasicComponent {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeIndex: props.initialIndex || 0
-    };
-  }
-
   componentDidMount() {
     super.componentDidMount();
     const node = this.refs.tabbar;
     CustomElements.upgrade(node);
-    node.setActiveTab(this.state.activeIndex);
-    node.addEventListener('prechange', this.handleChange.bind(this));
+
+    node.setActiveTab(this.props.activeIndex);
+
     node.addEventListener('prechange', this.props.onPreChange);
     node.addEventListener('postchange', this.props.onPostChange);
     node.addEventListener('reactive', this.props.onReactive);
@@ -24,26 +18,27 @@ class Tabbar extends BasicComponent {
 
   componentWillUnmount() {
     const node = this.refs.tabbar;
-    node.removeEventListener('prechange', this.handleChange);
+    // node.removeEventListener('prechange', this.handleChange);
     node.removeEventListener('prechange', this.props.onPreChange);
     node.removeEventListener('postchange', this.props.onPostChange);
     node.removeEventListener('reactive', this.props.onReactive);
   }
 
-  handleChange(event) {
-    this.setState({activeIndex: event.index});
-  }
-
-  setActiveTab(index, options) {
-    this.refs.tabbar.setActiveTab(index, options);
-  }
-
-  getActiveTabIndex() {
-    return this.refs.tabbar.getActiveTabIndex();
-  }
+  //
+  // handleChange(event) {
+  //   this.setState({activeIndex: event.index});
+  // }
+  //
+  // setActiveTab(index, options) {
+  //   this.refs.tabbar.setActiveTab(index, options);
+  // }
+  //
+  // getActiveTabIndex() {
+  //   return this.refs.tabbar.getActiveTabIndex();
+  // }
 
   render() {
-    const tabs = this.props.renderTabs(this.state.activeIndex, this);
+    const tabs = this.props.renderTabs(this.props.activeIndex, this);
 
     var {...others} = this.props;
 
@@ -54,7 +49,7 @@ class Tabbar extends BasicComponent {
     Util.convert(others, 'animationOptions', {fun: Util.animationOptionsConverter, newName: 'animation-options'});
 
     return (
-      <ons-tabbar {...this.props} ref='tabbar' activeIndex={this.state.activeIndex} _compiled='true'>
+      <ons-tabbar {...this.props} ref='tabbar' activeIndex={this.props.activeIndex} _compiled='true'>
         <div no-status-bar-fill className={'ons-tab-bar__content tab-bar__content' + (this.props.position === 'top' ? ' tab-bar--top__content' : '')}>
           {tabs.map((tab) => tab.content)}
         </div>
@@ -67,7 +62,7 @@ class Tabbar extends BasicComponent {
 };
 
 Tabbar.propTypes = {
-  initialIndex: React.PropTypes.number.isRequired,
+  activeIndex: React.PropTypes.number.isRequired,
   renderTabs: React.PropTypes.func.isRequired,
   position: React.PropTypes.string,
   animation: React.PropTypes.oneOf(['none', 'slide', 'fade']),
@@ -75,10 +70,6 @@ Tabbar.propTypes = {
   onPreChange: React.PropTypes.func,
   onPostChange: React.PropTypes.func,
   onReactive: React.PropTypes.func
-};
-
-Tabbar.defaultProps = {
-  initialIndex: 0
 };
 
 export default Tabbar;
